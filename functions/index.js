fe45449942f0c052a4e2645965a5819b34caf726
Isha Pandya <Isha.Pandya@team.telstra.com>
2017-07-29 16:22:24 +1000

    adding update and delete function

--- functions/index.js
+++ functions/index.js
@@ -9,11 +9,56 @@ exports.registerUser = functions.https.onRequest((request, response) => {
   // save to DB
   var path = "users/" + request.body.userId + "/";
   var reference = admin.database().ref(path);
-  reference.set({
-    userDay: request.body.userDay,
-    userTime: request.body.userTime,
-    userCollectionDay: request.body.userCollectionDay
-  });
+  reference.set(
+    {
+      userDay: request.body.userDay,
+      userTime: request.body.userTime,
+      userCollectionDay: request.body.userCollectionDay
+    },
+    function(error) {
+      if (error) {
+        console.log("Data could not be saved." + error);
+      } else {
+        console.log("Data saved successfully.");
+      }
+    }
+  );
 
   response.send("User Registered successfully!");
+});
+
+exports.deleteUser = functions.https.onRequest((request, response) => {
+  // save to DB
+  var path = "users/" + request.body.userId + "/";
+  var reference = admin.database().ref(path);
+  reference.set({}, function(error) {
+    if (error) {
+      console.log("Data could not be deleted." + error);
+    } else {
+      console.log("Data Deleted successfully.");
+    }
+  });
+
+  response.send("User Deleted successfully!");
+});
+
+exports.updateUser = functions.https.onRequest((request, response) => {
+  // save to DB
+  var path = "users/" + request.body.userId + "/";
+  var reference = admin.database().ref(path);
+  reference.update(
+    {
+      userDay: request.body.userDay,
+      userTime: request.body.userTime,
+      userCollectionDay: request.body.userCollectionDay
+    },
+    function(error) {
+      if (error) {
+        console.log("Data could not be updated." + error);
+      } else {
+        console.log("Data update successfully.");
+      }
+    }
+  );
+  response.send("User details updated successfully!");
 });
