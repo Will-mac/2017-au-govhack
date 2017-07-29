3669a8f5ba7876df17c8fabde1ae186107fd73d8
Isha Pandya <Isha.Pandya@team.telstra.com>
2017-07-29 15:15:35 +1000

    User data adds succesfully to DB

--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,19 @@
 const functions = require("firebase-functions");
-
+const admin = require("firebase-admin");
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 
+admin.initializeApp(functions.config().firebase);
+
 exports.registerUser = functions.https.onRequest((request, response) => {
-  var userId = request.body.userId;
-  var userDay = request.body.userDay;
-  var userTime = request.body.userTime;
-  var userCollectionDay = request.body.userCollectionDay;
+  // save to DB
+  var path = "users/" + request.body.userId + "/";
+  var reference = admin.database().ref(path);
+  reference.set({
+    userDay: request.body.userDay,
+    userTime: request.body.userTime,
+    userCollectionDay: request.body.userCollectionDay
+  });
 
-  var a = " userId " + userId;
-  a += "  userDay " + userDay;
-  a += "  userTime " + userTime;
-  a += "  userCollectionDay " + userCollectionDay;
-  // commit the datetime
-
-  response.send("User Registered successfully!" + a);
+  response.send("User Registered successfully!");
 });
