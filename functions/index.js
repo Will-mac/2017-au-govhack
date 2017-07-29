d062ba8841f0a5b958b3ef05580d7e6a274595a0
Unknown <jthegedus@jttech.io>
2017-07-29 20:21:43 +1000

    Auto stash before merge of "google-server-cron" and "master"

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,9 @@
-const functions = require("firebase-functions");
-const admin = require("firebase-admin");
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
+const functions = require("firebase-functions")
+const admin = require("firebase-admin")
+const secureCompare = require("secure-compare")
 
-admin.initializeApp(functions.config().firebase);
+// init the db with admin privileges
+admin.initializeApp(functions.config().firebase)
 
 exports.registerUser = functions.https.onRequest((request, response) => {
   var path = "users/" + request.body.userId + "/";
@@ -38,6 +38,41 @@ exports.deleteUser = functions.https.onRequest((request, response) => {
 
   response.send("User Deleted successfully!");
 });
+
+/**
+ * this is called from a Zapier timed webhook - https://zapier.com/zapbook/webhook/
+ * When requested this Function will delete every user accounts that has been inactive for 30 days.
+ * The request needs to be authorized by passing a 'key' query parameter in the URL. This key must
+ * match a key set as an environment variable using `firebase functions:config:set cron.key="YOUR_KEY"`.
+ */
+exports.sendNotification = functions.https.onRequest((request, response) => {
+  // check the Security Key used by 3rd Parties to trigger this function
+  const key = request.query.key
+
+  // Exit if the keys don't match
+  if (!secureCompare(key, functions.config().cron.key)) {
+    console.log(
+      "The key provided in the request does not match the key set in the environment. Check that",
+      key,
+      "matches the cron.key attribute in `firebase env:get`"
+    )
+    response
+      .status(403)
+      .send(
+        'Security key does not match. Make sure your "key" URL query parameter matches the ' +
+          "cron.key environment variable."
+      )
+    return
+  }
+
+  //
+
+  // get current UTC time
+  // query db for all users, filter on time by converting the userDay and userTime to UTC
+  // call FCM with all users data
+  console.log("Triggered from Zapier")
+  response.send("Notification function sent successfully!")
+})
 
 exports.updateUser = functions.https.onRequest((request, response) => {
   var path = "users/" + request.body.userId + "/";
